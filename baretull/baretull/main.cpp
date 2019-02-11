06a7a6ee130225a694c1d0be12af8c7f10bb6f01
neverlien <neverlien@gmail.com>
2019-02-11 19:54:35 +0100

    helloo

---
+++ baretull/baretull/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  baretull
+//
+//  Created by Åse Victoria Neverlien on 11/02/2019.
+//  Copyright © 2019 Åse Victoria Neverlien. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
