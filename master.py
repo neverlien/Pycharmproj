19e1c1c8093431a2140e467ae2512ca16e309b6b
Aase <asenev@OSLL101429.verit.dnv.com>
2021-08-13 08:30:12 +0200

    testing something

--- master.py
+++ master.py
@@ -1,13 +1,16 @@
-import random
-import tensorflow as tf
-import keras
+# This is a sample Python script.
 
-x=10
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-if x <=7:
 
-    print("first")
-elif x == 8:
-    print("nei")
-else:
-    print("heloo")
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('test')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
